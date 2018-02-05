fa478841998212f5e106fa6e6a4282b56268e527
Cameron Mkj <cameronmkj@gmail.com>
2018-02-05 03:38:37 +0000

    Pushing to ccheck on VM.

--- main.py
+++ main.py
@@ -1,5 +1,26 @@
 import os, time
 
+os.system("clear")
+payload = raw_input("What payload are you going to be using? ")
+os.system("clear")
+ip = raw_input("What is your LHOST? ")
+os.system("clear")
+port = raw_input("What is your LPORT? ")
+os.system("clear")
+exploit = raw_input("Do you want to run the exploit immediately? (Y/N) ")
+exploitTwo = exploit.lower()
+
 os.system("msfconsole")
+print("Script Waiting 10 seconds to allow the Metasploit Console to load fully.")
 time.sleep(10)
-os.system("")
+os.system("use exploit/multi/handler")
+os.system("set payload " + payload)
+os.system("set LHOST " + ip)
+os.system("set lport " + port)
+if exploitTwo == "y":
+    os.system("exploit -j -z")
+elif exploitTwo == "n":
+    pass
+
+else:
+    print("That is not an allowed response.")
